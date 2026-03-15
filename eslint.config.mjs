ba55dcbed1ddf0b62633141d51b44e3ff9767991
ASHWAGANDHUM <agq772@gmail.com>
2026-03-15 22:11:37 +0800

    add .gitignore and deletre node_modules folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import pluginJs from '@eslint/js'
 import config from 'eslint-config-prettier'
 import plugin from 'eslint-plugin-prettier/recommended'
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
